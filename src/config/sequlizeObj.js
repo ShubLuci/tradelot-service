d3b7f4bad99683f91680b59f23b8edde96728e9e
Shubham Yadav <cshubyadav@gmail.com>
2025-05-01 13:55:16 +0530

    SSL Server check

--- src/config/sequlizeObj.js
+++ src/config/sequlizeObj.js
@@ -8,7 +8,13 @@ const sequelize = new Sequelize(
     {
         host: process.env.DB_HOST,
         dialect: 'postgres',
-        logging: false
+        logging: false,
+        dialectOptions: {
+            ssl: {
+              require: true,
+              rejectUnauthorized: false // For development; true if using a trusted CA in production
+            }
+        }
     }
 );
 
